b8c8b7de423dfb057ec3563e6407c7731fe695d1
KaranManku <58914710+Karanmanku4@users.noreply.github.com>
2020-09-14 20:02:48 -0400

    - Integrated dip when drawing custom views

--- app/src/main/java/com/example/composingapp/views/BarViewGroup.java
+++ app/src/main/java/com/example/composingapp/views/BarViewGroup.java
@@ -5,19 +5,26 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.widget.LinearLayout;
 
 class BarViewGroup extends LinearLayout {
     private static final String TAG = "BarViewGroup";
     private Paint mBarPaint;
-    private float mBarLineInPx;
-    private int mBarX, mBarY, mBarWidth, mBarHeight;
+
+    // Measurements for musical bar drawing in dip
+    private static float mBarLineSize = 5;
+    private static float mBarX = 50;
+    private static float mBarY = 50;
+    private static float mBarWidth = 2000;
+    private static float mBarHeight = 100;
+
 
     public BarViewGroup(Context context, AttributeSet attrs) {
         super(context, attrs);
         setOrientation(LinearLayout.HORIZONTAL);
 
-        initBarDimensions();
+        convertMeasuresToPx();
         initBarPaint();
     }
 
@@ -39,14 +46,26 @@ class BarViewGroup extends LinearLayout {
     }
 
     /**
-     * Initializes the values of the member variables. Dictates the size of the bar on the screen
+     * Converts from dp to px
+     *
+     * @param dp (float) measure of density-independent pixel
+     * @return (float) measure of dp in pixels for the operating device
      */
-    private void initBarDimensions() {
-        mBarLineInPx = 5;
-        mBarX = 50;
-        mBarY = 50;
-        mBarHeight = 100;
-        mBarWidth = 400;
+    private float convertDpToPx(float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dp, getResources().getDisplayMetrics());
+    }
+
+    /**
+     * Re-assigns measurements of bar components by converting from dp to px to support multiple
+     * devices
+     */
+    private void convertMeasuresToPx() {
+        mBarLineSize = convertDpToPx(mBarLineSize);
+        mBarX = convertDpToPx(mBarX);
+        mBarY = convertDpToPx(mBarY);
+        mBarHeight = convertDpToPx(mBarHeight);
+        mBarWidth = convertDpToPx(mBarWidth);
     }
 
     /**
@@ -56,17 +75,17 @@ class BarViewGroup extends LinearLayout {
         mBarPaint = new Paint();
         mBarPaint.setStyle(Paint.Style.STROKE);
         mBarPaint.setColor(Color.parseColor("black"));
-        mBarPaint.setStrokeWidth(mBarLineInPx);
+        mBarPaint.setStrokeWidth(mBarLineSize);
     }
 
     /**
      * Draws bar-lines on a canvas
      *
-     * @param canvas The canvas to draw on
+     * @param canvas (Canvas) The canvas to draw on
      */
     private void drawBarLines(Canvas canvas) {
         // Position variable for the y-coordinate since it is the only one that changes in each loop
-        int currentY = mBarY;
+        float currentY = mBarY;
         // Draw the 5 lines from top to bottom
         for (int i = 0; i < 5; i++) {
             canvas.drawLine(mBarX, currentY, mBarX + mBarWidth, currentY, mBarPaint);
