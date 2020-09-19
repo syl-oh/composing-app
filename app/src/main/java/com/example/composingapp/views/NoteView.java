4339ec593639fb83a83da36ce24df1bee9a12e3d
KaranManku <58914710+Karanmanku4@users.noreply.github.com>
2020-09-19 17:54:16 -0400

    - Created drawNote() in NoteView which draws a note inside the view

--- app/src/main/java/com/example/composingapp/views/NoteView.java
+++ app/src/main/java/com/example/composingapp/views/NoteView.java
@@ -13,44 +13,26 @@ import androidx.annotation.Nullable;
 public class NoteView extends View {
     private static final String TAG = "NoteView";
     private Paint mNotePaint;
-    private int mNoteX, mNoteY;
+    private Paint mStemPaint;
+    private float mNoteX, mNoteY;
+    private float[] mNoteYPositions;
     private float mNoteRadius;
     private int canvasHeight;
     private int canvasWidth;
+    private float mStemWidth;
+    private float mStemHeight;
+
 
     public NoteView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        initNotePaint();
-    }
-
-    private void initMeasurements() {
-        mNoteX = canvasWidth / 2;
-        mNoteY = canvasHeight / 2;
-        mNoteRadius = 30;
-    }
-
-    private void initNotePaint() {
-        mNotePaint = new Paint();
-        mNotePaint.setColor(Color.parseColor("black"));
-        mNotePaint.setAntiAlias(true);
-    }
-
-    /**
-     * Converts from dp to px
-     *
-     * @param dp (float) measure of density-independent pixel
-     * @return (float) measure of dp in pixels for the operating device
-     */
-    private float convertDpToPx(float dp) {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                dp, getResources().getDisplayMetrics());
+        initNoteYPositions();
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         canvasWidth = w;
         canvasHeight = h;
-        initMeasurements();
+        initDrawMeasurements();
         super.onSizeChanged(w, h, oldw, oldh);
     }
 
@@ -62,6 +44,66 @@ public class NoteView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawCircle(mNoteX, mNoteY, mNoteRadius, mNotePaint);
+        drawNote(canvas);
+    }
+
+    private void drawNote(Canvas canvas) {
+        float leftX = (canvasWidth / 2) - mNoteRadius;
+        float rightX = (canvasWidth / 2) + mNoteRadius;
+        float topY = (canvasHeight / 2) + ((mNoteRadius * 3) / 4);
+        float bottomY = (canvasHeight / 2) - ((mNoteRadius * 3) / 4);
+        float midY = (topY + bottomY) / 2;
+
+
+        canvas.drawOval(leftX, topY, rightX, bottomY, mNotePaint);
+        canvas.drawLine(rightX - mStemWidth / 2,
+                midY,
+                rightX - mStemWidth / 2,
+                midY - mStemHeight,
+                mStemPaint);
+    }
+
+    private void initDrawMeasurements() {
+        mNoteX = canvasWidth / 2;
+        mNoteY = canvasHeight / 2;
+        mNoteRadius = canvasWidth / 20;
+        mStemHeight = (canvasHeight / 3);
+        mStemWidth = 3;
+
+        mNoteX = (int) convertDpToPx(mNoteX);
+        mNoteY = (int) convertDpToPx(mNoteY);
+        mNoteRadius = convertDpToPx(mNoteRadius);
+        mStemWidth = convertDpToPx(mStemWidth);
+
+        initNotePaint();
+    }
+
+    private void initNoteYPositions() {
+        mNoteYPositions = new float[33];
+        float highestY = mStemHeight;
+        for (int i = 1; i <= 33; i++) {
+            mNoteYPositions[i - 1] = highestY + (i / 33) * canvasHeight;
+        }
+    }
+
+    private void initNotePaint() {
+        mNotePaint = new Paint();
+        mNotePaint.setColor(Color.parseColor("black"));
+        mNotePaint.setAntiAlias(true);
+
+        mStemPaint = new Paint();
+        mStemPaint.setColor(Color.parseColor("black"));
+        mStemPaint.setStrokeWidth(mStemWidth);
+    }
+
+    /**
+     * Converts from dp to px
+     *
+     * @param dp (float) measure of density-independent pixel
+     * @return (float) measure of dp in pixels for the operating device
+     */
+    private float convertDpToPx(float dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dp, getResources().getDisplayMetrics());
     }
 }
