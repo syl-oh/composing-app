cbf5c48fbc6f5164096d54a92c5e27764388a866
Karan Manku <k.manku4@gmail.com>
2021-01-09 18:40:18 -0500

    Added menu/undo/redo buttons to layout Created RecyclerView for user buttons

--- app/src/main/java/com/example/composingapp/activities/MainActivity.java
+++ app/src/main/java/com/example/composingapp/activities/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.composingapp.activities;
 
 import android.os.Bundle;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
@@ -9,17 +8,9 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.composingapp.R;
-import com.example.composingapp.utils.interfaces.ui.Clickable;
-import com.example.composingapp.utils.music.BarObserver;
-import com.example.composingapp.utils.music.Music;
-import com.example.composingapp.utils.music.Note;
-import com.example.composingapp.utils.music.NoteTable;
 import com.example.composingapp.viewmodels.ScoreViewModel;
-import com.example.composingapp.views.NoteView;
 import com.example.composingapp.views.ScoreLineAdapter;
 import com.example.composingapp.views.ScoreLineView;
-import com.example.composingapp.views.commands.ChangeClefCommand;
-import com.example.composingapp.views.commands.ChangeNoteCommand;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
@@ -47,27 +38,27 @@ public class MainActivity extends AppCompatActivity {
                 .observe(this, scoreLineAdapter::setScoreObservable);
 
 
-        ImageButton quarterNoteButton = findViewById(R.id.quarterNoteButton);
-        quarterNoteButton.setOnClickListener(view -> {
-            Clickable clickedChild = scoreLineView.findClickedChild();
-            if (clickedChild instanceof NoteView) {
-                Note oldNote = ((NoteView) clickedChild).getNotePositionDict().getNote();
-                BarObserver barObserver = ((NoteView) clickedChild).getBarViewGroup().getBarObserver();
-                int index = ((NoteView) clickedChild).getBarViewGroup().getNoteViewList().indexOf(clickedChild);
-                Note replacement = NoteTable.get(oldNote.getPitchClass(), oldNote.getOctave(), Music.NoteLength.QUARTER_NOTE);
-                ChangeNoteCommand command =
-                        new ChangeNoteCommand(mScoreViewModel, barObserver, index, replacement);
-                command.execute();
-                command.notifyScoreViewModelObservers();
-            }
-        });
-
-        ImageButton bassClefButton = findViewById(R.id.bassClefButton);
-        ChangeClefCommand bassClefCommand = new ChangeClefCommand(mScoreViewModel, Music.Clef.BASS_CLEF);
-        bassClefButton.setOnClickListener(v -> bassClefCommand.execute());
-
-        ImageButton trebleClefButton = findViewById(R.id.trebleClefButton);
-        ChangeClefCommand trebleClefCommand = new ChangeClefCommand(mScoreViewModel, Music.Clef.TREBLE_CLEF);
-        trebleClefButton.setOnClickListener(v -> trebleClefCommand.execute());
+//        ImageButton quarterNoteButton = findViewById(R.id.quarterNoteButton);
+//        quarterNoteButton.setOnClickListener(view -> {
+//            Clickable clickedChild = scoreLineView.findClickedChild();
+//            if (clickedChild instanceof NoteView) {
+//                Note oldNote = ((NoteView) clickedChild).getNotePositionDict().getNote();
+//                BarObserver barObserver = ((NoteView) clickedChild).getBarViewGroup().getBarObserver();
+//                int index = ((NoteView) clickedChild).getBarViewGroup().getNoteViewList().indexOf(clickedChild);
+//                Note replacement = NoteTable.get(oldNote.getPitchClass(), oldNote.getOctave(), Music.NoteLength.QUARTER_NOTE);
+//                ChangeNoteCommand command =
+//                        new ChangeNoteCommand(mScoreViewModel, barObserver, index, replacement);
+//                command.execute();
+//                command.notifyScoreViewModelObservers();
+//            }
+//        });
+//
+//        ImageButton bassClefButton = findViewById(R.id.redo_button);
+//        ChangeClefCommand bassClefCommand = new ChangeClefCommand(mScoreViewModel, Music.Clef.BASS_CLEF);
+//        bassClefButton.setOnClickListener(v -> bassClefCommand.execute());
+//
+//        ImageButton trebleClefButton = findViewById(R.id.undo_button);
+//        ChangeClefCommand trebleClefCommand = new ChangeClefCommand(mScoreViewModel, Music.Clef.TREBLE_CLEF);
+//        trebleClefButton.setOnClickListener(v -> trebleClefCommand.execute());
     }
 }
--- app/src/main/java/com/example/composingapp/views/commands/ChangeNoteCommand.kt
+++ app/src/main/java/com/example/composingapp/views/commands/ChangeNoteCommand.kt
@@ -4,7 +4,6 @@ import com.example.composingapp.utils.interfaces.ui.Command
 import com.example.composingapp.utils.music.BarObserver
 import com.example.composingapp.utils.music.Note
 import com.example.composingapp.viewmodels.ScoreViewModel
-import com.example.composingapp.viewmodels.ViewModelHelper.mutation
 
 class ChangeNoteCommand(
         private val scoreViewModel: ScoreViewModel,
@@ -23,9 +22,5 @@ class ChangeNoteCommand(
 
     override fun undo() {
         TODO("Not yet implemented")
-    }
-
-    fun notifyScoreViewModelObservers() {
-        scoreViewModel.scoreObservableMutableLiveData.mutation {  }
     }
 }
